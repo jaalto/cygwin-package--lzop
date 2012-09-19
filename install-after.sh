57c09dfc6c655a2defdf4a608fb81cc460899586
Jari Aalto <jari.aalto@cante.net>
2012-09-19 08:46:58 +0300

    1.03-1
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

--- install-after.sh
+++ install-after.sh
@@ -23,21 +23,21 @@ Main()
     if [ "$root"  ] && [ -d $root ]; then
 
         root=$(echo $root | sed 's,/$,,')  # Delete trailing slash
-        docdir=$(cd $root/usr/share/doc/lzop-* && pwd)
+        docdir=$(cd $root/usr/share/doc/lzop && pwd)
 
         #  For some reason the install process copies files
         #  as symlinks to docdir.
 
-        echo "... Unbreaking the symlinks and using real files"
+        echo ">> Unbreaking the symlinks and using real files"
 
         for file in $docdir/*
         do
           [ -h $file ] || continue
 
           #  Remove symlinks and copy real files
-          Cmd cp $file $file.tmp
-          Cmd rm $file
-          Cmd mv $file.tmp $file
+          Cmd cp "$file" "$file.tmp"
+          Cmd rm "$file"
+          Cmd mv "$file.tmp" "$file"
         done
     fi
 }
